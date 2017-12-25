b709f3cccba7c73cd787bf71c10c2c85a3b6c3c3
sauyee <sauyee333@gmail.com>
2017-12-25 17:52:16 +0800

    initial commit

---
+++ app/src/androidTest/java/za/co/riggaroo/gus/ExampleInstrumentedTest.java
@@ -0,0 +1,31 @@
+package za.co.riggaroo.gus;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        //for mock flavor
+        assertEquals("za.co.riggaroo.gus.mock", appContext.getPackageName());
+
+        //for prod
+//        assertEquals("za.co.riggaroo.gus", appContext.getPackageName());
+
+    }
+}
